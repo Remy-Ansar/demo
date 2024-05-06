c3aa28afe185394866a4a2447d0e37ffb3db7639
Xelaj <ajuillard69@gmail.com>
2024-05-06 14:58:23 +0200

    feat(traits): add traits for dateTime & Enable

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -2,28 +2,30 @@
 
 namespace App\Entity;
 
-use App\Repository\CategorieRepository;
+use App\Entity\Traits\DateTimeTrait;
+use App\Entity\Traits\EnableTrait;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategorieRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Categorie
 {
+    use DateTimeTrait;
+    use EnableTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[UniqueEntity(fields: ['name'], message: 'Ce nom est déjà utilisé pour une autre catégorie')]
     private ?string $name = null;
-
-    #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?\DateTimeImmutable $updatedAt = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?bool $enable = null;
 
     public function getId(): ?int
     {
@@ -38,30 +40,6 @@ class Categorie
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
-    {
-        $this->updatedAt = $updatedAt;
 
         return $this;
     }
